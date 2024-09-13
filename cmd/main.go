4011722715eaa753f67281a07e4606dba026102d
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-09-13 23:25:55 +0300

    feat: initial project

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", "Hello world!")
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
